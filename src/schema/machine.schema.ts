e008de2c080304f1ab7a9b4964dc627e28c6e1a4
SriranganK <srirangankannan31@gmail.com>
2025-05-31 22:04:35 +0530

    reverted back the changes made in transaction endpoint

--- src/schema/machine.schema.ts
+++ src/schema/machine.schema.ts
@@ -5,6 +5,7 @@ import {z} from "zod";
 export const machineSchema = z.object({
   to: z.string(),
   amount: z.string(),
+  txId: z.string().optional(),
   password: z.string(),
   paymentMethod: z.string(),
   currency: z.string()
